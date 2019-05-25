2b6fd4c49d0268411402684b246848c62632131a
ThangVu <thangvubk@gmail.com>
2019-05-25 04:10:50 +0000

    remove unused configs

--- configs/cascade_rpn/cascade_rpn_r50_fpn_1x.py
+++ configs/cascade_rpn/cascade_rpn_r50_fpn_1x.py
@@ -2,14 +2,15 @@
 model = dict(
     type='CascadeRPN',
     num_stages=2,
-    pretrained='modelzoo://resnet50',
+    pretrained='open-mmlab://resnet50_caffe',
     backbone=dict(
         type='ResNet',
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        style='pytorch'),
+        normalize=dict(type='BN', frozen=True),
+        style='caffe'),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
@@ -17,25 +18,29 @@ model = dict(
         num_outs=5),
     rpn_head=[
         dict(
-            type='RPNHead',
+            type='CascadeRPNHead',
             in_channels=256,
             feat_channels=256,
             anchor_scales=[8],
             anchor_ratios=[1.0],
             anchor_strides=[4, 8, 16, 32, 64],
             target_means=[.0, .0, .0, .0],
-            target_stds=[1.0, 1.0, 1.0, 1.0],
-            use_sigmoid_cls=True,
-            feat_adapt=True),
+            target_stds=[0.1, 0.1, 0.5, 0.5],
+            # args for cascade_rpn
+            with_cls=False,
+            dilation=3,
+            bridged_feature=True,
+            sampling=False),
         dict(
-            type='RPNHead',
+            type='CascadeRPNHead',
             in_channels=256,
             feat_channels=256,
             anchor_scales=[8],
             anchor_ratios=[1.0],
             anchor_strides=[4, 8, 16, 32, 64],
             target_means=[.0, .0, .0, .0],
-            target_stds=[1.0, 1.0, 1.0, 1.0],
+            target_stds=[0.05, 0.05, 0.1, 0.1],
+            # args for cascade_rpn
             use_sigmoid_cls=True,
             feat_adapt=True)
     ])
@@ -44,26 +49,18 @@ train_cfg = dict(
     rpn=[
         dict(
             assigner=dict(
-                type='MaxIoUAssigner',
-                pos_iou_thr=0.6,
-                neg_iou_thr=0.3,
-                min_pos_iou=0.3,
-                ignore_iof_thr=-1),
-            sampler=dict(
-                type='RandomSampler',
-                num=256,
-                pos_fraction=0.5,
-                neg_pos_ub=-1,
-                add_gt_as_proposals=False),
-            allowed_border=0,
+                type='RegionAssigner',
+                center_ratio=0.2,
+                ignore_ratio=0.5),
+            allowed_border=-1,
             pos_weight=-1,
-            smoothl1_beta=1 / 9.0,
+            bbox_loss=dict(type='IoU', reg_ratio=10),
             debug=False),
         dict(
             assigner=dict(
                 type='MaxIoUAssigner',
                 pos_iou_thr=0.7,
-                neg_iou_thr=0.3,
+                neg_iou_thr=0.7,
                 min_pos_iou=0.3,
                 ignore_iof_thr=-1),
             sampler=dict(
@@ -72,24 +69,24 @@ train_cfg = dict(
                 pos_fraction=0.5,
                 neg_pos_ub=-1,
                 add_gt_as_proposals=False),
-            allowed_border=0,
+            allowed_border=-1,
             pos_weight=-1,
-            smoothl1_beta=1 / 9.0,
-            debug=False)
-    ])
+            bbox_loss=dict(type='IoU', reg_ratio=10),
+            debug=False)],
+    rpn_stage_loss_weights=[1, 1])
 test_cfg = dict(
     rpn=dict(
         nms_across_levels=False,
         nms_pre=2000,
         nms_post=2000,
         max_num=2000,
-        nms_thr=0.7,
+        nms_thr=0.8,
         min_bbox_size=0))
 # dataset settings
 dataset_type = 'CocoDataset'
 data_root = 'data/coco/'
 img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+    mean=[102.9801, 115.9465, 122.7717], std=[1.0, 1.0, 1.0], to_rgb=False)
 data = dict(
     imgs_per_gpu=2,
     workers_per_gpu=2,
@@ -142,7 +139,7 @@ log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
-        # dict(type='TensorboardLoggerHook')
+        dict(type='TensorboardLoggerHook')
     ])
 # yapf:enable
 # runtime settings
